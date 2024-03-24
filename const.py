c1847584d17df079acb164a0be2afc5075db67d1
IvanSlivenko <ivan8822@ukr.net>
2024-03-24 21:24:18 +0300

    24-03-2024-21-24

--- const.py
+++ const.py
@@ -1,1 +1,8 @@
 URL = 'https://scrapingclub.com/exercise/list_basic/?page=1'
+URL_F = 'https://scrapingclub.com/exercise/list_basic/?page='
+URL_SHORT = 'https://scrapingclub.com'
+HEADERS_LIST= {
+                'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36 OPR/108.0.0.0',
+
+
+}
--- parser.py
+++ parser.py
@@ -1,20 +1,38 @@
 import requests
 from bs4 import BeautifulSoup
+from time import sleep
+from const import URL, URL_SHORT, URL_F, HEADERS_LIST
 
-from const import URL
+list_card_url = []
 
-response = requests.get(URL)
+total_count = input('Вкажіть кількість сторінок для пасингу : \n')
 
-# Перевіряємо доступ до сайту
-# print(response)
-# print(response.text)
+for count in range(1, int(total_count)+1):
+    sleep(3)# pausa == 3 sec
+    print('-------------------------------------Парсимо сторінку :', count)
+    url = f"{URL_F}+{count}"
+    response = requests.get(url, headers=HEADERS_LIST)
+    soup = BeautifulSoup(response.text, "lxml") #html.parser
+    data = soup.find_all("div",class_="w-full rounded border")
 
-soup = BeautifulSoup(response.text, "lxml") #html.parser
-# print(soup)
+    for i in data:
+        card_url = URL_SHORT + i.find("a").get('href')
+        list_card_url.append(card_url)
+    # print(list_card_url)
 
-data = soup.find("div",class_="w-full rounded border")
-name = data.find('h4').text.replace("\n","")
-price = data.find('h5').text.replace("$","")
 
-print(name)
-print(price)
+for card_url in list_card_url:
+    response = requests.get(card_url, headers=HEADERS_LIST)
+    soup = BeautifulSoup(response.text, "lxml")  # html.parser
+    data_card = soup.find("div", class_="my-8 w-full rounded border")
+    name = data_card.find('h3').text
+    print(name)
+
+
+        # name = i.find('h4').text.replace("\n","")
+        # price = i.find('h5').text.replace("$","")
+        # url_img = URL_SHORT + i.find("img", class_="card-img-top img-fluid").get("src")
+        # print(name+"\n"+price+"\n"+url_img+"\n\n")
+
+print('Парсинг сторінок завершено')
+
